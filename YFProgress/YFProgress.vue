273c3c17ccab85f4cd48d1103ef3724558683845
Yangfan <15234408101@163.com>
2018-02-08 09:25:26 +0800

    Update YFProgress.vue

--- YFProgress/YFProgress.vue
+++ YFProgress/YFProgress.vue
@@ -78,5 +78,29 @@
             tpl && ((tpl.remove) ? (tpl.remove()) : (tpl.parentNode && tpl.parentNode.removeChild(tpl)));
         }
     });
-
 </script>
+<style>
+.y-progress {
+    position: relative;
+    width: 100%;
+    height: 8px;
+    background-color: #ccc;
+    border-radius: 4px;
+    overflow: hidden;
+}
+.y-progress_bar {
+    position: absolute;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    border-radius: 4px;
+    transition: all 1.5s ease;
+}
+.y-progress_text {
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    text-align: center;
+}
+</style>
